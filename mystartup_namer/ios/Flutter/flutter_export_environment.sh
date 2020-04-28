9ff48397f030388abbedef0f1ddaa935660ad8cb
OmarBurney <yenrubramo@gmail.com>
2020-04-27 22:44:17 -0700

    Add Dart, Ios, And Android Dependencies

---
+++ mystartup_namer/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Omar\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Omar\Desktop\Flutter_Workshop_Part_1\mystartup_namer"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Omar\Documents\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
